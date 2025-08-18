cfde36a9357f578a282d4f6e864809a489c79c60
rshaurya24 <shaurya.r.pethe@gmail.com>
2025-08-18 21:20:03 +0530

    Changed name from 'Static' to 'static' according to Flask conventions

---
+++ static/js/autocompleteData.js
@@ -0,0 +1,7 @@
+// static/js/autocompleteData.js
+window.MED_SUGGESTIONS = [
+  "Itching","Vomiting","Nausea","Headache","Cough","Fever","Cold","Fatigue",
+  "Dizziness","Chest pain","Shortness of breath","Diabetes","Hypertension",
+  "Asthma","Allergy","Sore throat","Back pain","Diarrhea","Constipation",
+  "Anxiety","Depression"
+];
